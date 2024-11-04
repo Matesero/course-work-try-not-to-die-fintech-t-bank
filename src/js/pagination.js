88f42195defd86f821099a61eca9865a0bb2956f
Matesero <grishenkov05@mail.ru>
2024-11-04 10:47:36 +0700

    edit pagination

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -25,27 +25,56 @@ export class Pagination {
         const paginationContainer = document.getElementById("pagination-container");
         paginationContainer.innerHTML = '';
 
-        for (let i = 1; i <= pageCount; i++) {
-            const pageLink = document.createElement("a");
-            const params = new URLSearchParams('');
-            pageLink.textContent = i;
+        let start = Math.max(1, this.page - 2);
+        const end = Math.min(start + 4, pageCount)
 
-            if (this.name) params.append("name", this.name);
-            if (this.conclusions) params.append("conclusions", this.conclusions);
-            if (this.sorting) params.append("sorting", this.sorting);
-            if (this.scheduledVisits) params.append("scheduledVisits", this.scheduledVisits);
-            if (this.onlyMine) params.append("onlyMine", this.onlyMine);
-            params.append("page", i);
-            params.append("size", this.size);
-            pageLink.href = `?${params.toString()}`;
+        if (end - start < 4 && start !== 1) {
+            start = end - 4;
+        }
 
-            if (i === this.page) {
-                pageLink.classList.add("active");
-            }
+        const params = new URLSearchParams('');
+        if (this.name) params.append("name", this.name);
+        if (this.conclusions) params.append("conclusions", this.conclusions);
+        if (this.sorting) params.append("sorting", this.sorting);
+        if (this.scheduledVisits) params.append("scheduledVisits", this.scheduledVisits);
+        if (this.onlyMine) params.append("onlyMine", this.onlyMine);
+        const numberedPage = Number(this.page);
 
-            paginationContainer.appendChild(pageLink);
+        for (let i = start - 1; i <= end + 1; i++) {
+            const copyParams = new URLSearchParams(params.toString());
+            const pageBtn = renderPaginationButton(copyParams, i, numberedPage, this.size, pageCount, start, end);
+
+            if (i === numberedPage) pageBtn.classList.add("active");
+            if (i === pageCount + 1 && numberedPage === pageCount) pageBtn.classList.add('disabled')
+            if (i === 0 && numberedPage === 1) pageBtn.classList.add('disabled')
+
+            paginationContainer.appendChild(pageBtn);
         }
     }
+}
+
+function renderPaginationButton(params, page, currentPage, size, pageCount, start, end) {
+    const pageBtn = document.createElement("a");
+    pageBtn.classList.add("pagination__button");
+
+    switch (page) {
+        case start - 1:
+            pageBtn.textContent = "<";
+            params.append("page", currentPage - 1);
+            break
+        case end + 1:
+            pageBtn.textContent = ">";
+            params.append("page", currentPage + 1);
+            break
+        default :
+            pageBtn.textContent = page;
+            params.append("page", page);
+    }
+
+    params.append("size", size);
+    pageBtn.href = `?${params.toString()}`;
+
+    return pageBtn
 }
 
 export function getParams(){
