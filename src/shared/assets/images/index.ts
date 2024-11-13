3a2b7b7203c4cd95ad00c43fe20065c1f10c069f
Matesero <grishenkov05@mail.ru>
2024-11-14 04:19:25 +0700

    add navbar and images

---
+++ src/shared/assets/images/index.ts
@@ -0,0 +1,4 @@
+export { ReactComponent as ScullIcon } from './scull.svg';
+export { ReactComponent as BarsIcon } from './bars.svg';
+export { ReactComponent as FemaleIcon } from './female.svg';
+export { ReactComponent as MaleIcon } from './male.svg';
