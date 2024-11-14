fa22aef70291f6df087d2f7cab4b619cda46689f
Matesero <grishenkov05@mail.ru>
2024-11-15 03:34:47 +0700

    add loading component

--- src/pages/profile/ui/ProfilePage.tsx
+++ src/pages/profile/ui/ProfilePage.tsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import { Layout } from '~/app/layout';
 import { userFormsUi } from '~/features/user';
+import { medicalSystemApi } from '~/shared/api';
+import { useData } from '~/shared/hooks/useData';
+import { setUser } from '~/shared/store/user/store';
+import { sharedUiComponents } from '~/shared/ui';
+
+const { Loading } = sharedUiComponents;
+
+const { user } = medicalSystemApi;
 const { ProfileForm } = userFormsUi;
 
 export const ProfilePage = () => {
+    const dispatch = useDispatch();
+    const { data, isLoading } = useData(user.getProfile);
+
+    useEffect(() => {
+        if (data && data.data) {
+            dispatch(setUser(data.data));
+        }
+    }, [data, dispatch]);
+
     return (
-        <Layout isAuth={true}>
-            <ProfileForm />
+        <Layout isAuth={true} userName={data?.data?.name}>
+            {isLoading ? (
+                <Loading />
+            ) : (
+                <ProfileForm user={data?.data ?? null} />
+            )}
         </Layout>
     );
 };
